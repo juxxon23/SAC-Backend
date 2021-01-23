09b8d375c37068f50fd365ca0401b7fa66ecf2c7
NelsonM9 <nextnelson1234@gmail.com>
2021-01-23 03:08:39 -0500

    se crea controlador y sus complementos

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-from routes import signin, document
+from routes import login, signin, document
 from db.postgresql.model import db
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dwmdwuxrbsmznj:ced17943385fa0ef9f237f025f138b0200ebd897f32a01bc7d9908023bcebf7c@ec2-52-204-113-104.compute-1.amazonaws.com:5432/dc3cnsfmu7o2u'
 CORS(app, support_credentials= True)
 db.init_app(app)
+
+# Login routes
+app.add_url_rule(login['login'], view_func=login['view_func_login'])
 
 # Signin routes
 app.add_url_rule(signin['signin'], view_func=signin['view_func_signin'])
--- controllers/signin.py
+++ controllers/signin.py
@@ -3,15 +3,17 @@ from flask import jsonify, request
 from flask.views import MethodView
 from marshmallow import validate
 from helpers.encrypt_pass import Crypt
-from db.postgresql.model import User
-from validators.signin_u import RegisterUser, RegisterExtra
+from db.postgresql.model import User, Competencies, Results
+from validators.user_u import RegisterUser, RegisterExtra
 from db.postgresql.postgresql_manager import PostgresqlManager
+
 
 # Se incializan las variables con su respectivo metodo
 encrypt = Crypt()
 user_schema1 = RegisterUser()
 user_schema2 = RegisterExtra()
 postgres_tool = PostgresqlManager()
+
 
 class Signin(MethodView):
     def post(self):
@@ -20,24 +22,45 @@ class Signin(MethodView):
             errors = user_schema1.validate(user_signin)
             if errors:
                 return jsonify({'state': 'error', 'error': errors}), 403
-            document = postgres_tool.get_by(User, user_signin['email_inst'])
-            return jsonify({'state': 'ok'}), 203
+            document_cp = postgres_tool.get_by(User, user_signin['document_u'])
+            if document_cp != None:
+                return jsonify({'state': 'user exists'}), 403
+            new_user = User(
+                document_u=user_signin['document_u'],
+                email_inst=user_signin['email_inst'],
+                password_u=encrypt.hash_string(user_signin['password_u']),
+                name_u=user_signin['name_u'],
+                lastname_u=user_signin['lastname_u'],
+                phone_u=user_signin['phone_u'],
+                regional_u=user_signin['regional_u'],
+                center_u=user_signin['center_u'],
+                bonding_type=user_signin['bonding_type'],
+            )
+            new_competencies = Competencies(
+                document_user=user_signin['document_u'],
+                description=user_signin['description_c'],
+            )
+            new_results = Results(
+                document_user=user_signin['document_u'],
+                description=user_signin['description_r']
+            )
+            state = postgres_tool.add(new_user, new_competencies, new_results)
+            print('successfully registered')
+            return jsonify({'state': 'ok'})
         except:
-            return jsonify({'state': 'error in the form'}), 403
+            print('error in form')
+            return jsonify({'state': 'error'})
 
 
-
-#Se crea la clase SigninExtra
+# Se crea la clase SigninExtra
 class SigninExtra(MethodView):
 
     def put(self):
-        print(arreglo)
         try:
             users_signinEx = request.get_json()
             errors = user_schema2.validate(users_signinEx)
             if errors:
-                return ({'status': 'error', 'error': errors}),403
-            return jsonify({'state': 'ok'}),203
+                return ({'status': 'error', 'error': errors}), 403
+            return jsonify({'state': 'ok'}), 203
         except:
-            return jsonify({'status': 'error'}),403
-
+            return jsonify({'status': 'error'}), 403
