9b234d0c2d8fb5e381046ead34740d4aa0486e93
juxxon23 <juxxon23@hotmail.com>
2021-02-25 08:22:09 -0500

    Se actualiza model de la db PostgreSQL

--- app/db/postgresql/model.py
+++ app/db/postgresql/model.py
@@ -2,10 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     __tablename__ = 'User'
 
     document_u = db.Column(db.String(20), primary_key=True, nullable=False)
+    id_u = db.Column(db.String(10), nullable=False, unique=True)
     email_inst = db.Column(db.String(60), nullable=False)
     password_u = db.Column(db.String(128), nullable=False)
     name_u = db.Column(db.String(30), nullable=False)
@@ -15,10 +17,6 @@ class User(db.Model):
     regional_u = db.Column(db.String(100), nullable=False)
     center_u = db.Column(db.String(100), nullable=False)
     bonding_type = db.Column(db.Integer, db.ForeignKey('Bonding.id_bon'), nullable=False)
-    comp_fo = db.relationship(
-        'Competencies', backref='myComp', lazy='dynamic', foreign_keys='Competencies.document_user')
-    res_fo = db.relationship(
-        'Results', backref='myRes', lazy='dynamic', foreign_keys='Results.document_user')
 
     def __init__(self, document_u, email_inst, password_u, name_u, lastname_u, phone_u, city_u, regional_u, center_u, bonding_type):
         self.document_u = document_u
@@ -33,32 +31,6 @@ class User(db.Model):
         self.bonding_type = bonding_type
 
 
-class Competencies(db.Model):
-    __tablename__ = 'Competencies'
-
-    id_comp = db.Column(db.Integer, primary_key=True, nullable=False, index=True)
-    document_user = db.Column(db.String(20), db.ForeignKey('User.document_u'), nullable=False)
-    description = db.Column(db.String(255), nullable=False)
-
-
-    def __init__(self, document_user, description):
-        self.document_user = document_user
-        self.description = description
-
-
-class Results(db.Model):
-    __tablename__ = 'Results'
-
-    id_res = db.Column(db.Integer, primary_key=True, nullable=False, index=True)
-    document_user = db.Column(db.String(20), db.ForeignKey('User.document_u'), nullable=False)
-    comp_id = db.Column(db.Integer, db.ForeignKey('Competencies.id_comp'), nullable=False)
-    description = db.Column(db.String(255))
-
-    def __init__(self, document_user, comp_id, description):
-        self.document_user = document_user
-        self.comp_id = comp_id
-        self.description = description
-
 class Bonding(db.Model):
     __tablename__ = 'Bonding'
 
@@ -68,3 +40,14 @@ class Bonding(db.Model):
 
     def __init__(self, description):
         self.description = description
+
+
+class Paths(db.Model):
+    __tablename__ = 'Paths'
+
+    id_path = db.Column(db.Integer, primary_key=True, nullable=False)
+    path =  db.Column(db.String(255))
+    
+    def __init__(self, id_path, path):
+        self.path = id_path
+        self.path = path
