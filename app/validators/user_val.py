a457ef5918f86b0f877b7511e22f24a67734f8bf
NelsonM01 <nelson.morales@atlanticsoft.us>
2021-03-06 15:29:30 -0500

    ajuste en el validador de register user

--- app/validators/user_val.py
+++ app/validators/user_val.py
@@ -2,12 +2,23 @@ from marshmallow import Schema, fields, validate, validates, ValidationError
 
 
 class RegisterUser(Schema):
+
+    valid_email = ""
     email_inst = fields.String(
         required=True, validate=validate.Length(min=13, max=50))
     document_u = fields.String(
         required=True, validate=validate.Length(min=3, max=50))
     password_u = fields.String(
         required=True, validate=validate.Length(min=8, max=20))
+
+    @validates('email_inst')
+    def validate_email(self, email_inst):
+        self.valid_email = email_inst.split('@')
+        for i in self.valid_email:
+            if self.valid_email[1] == 'gmail.com' or self.valid_email[1] == 'misena.edu.co':
+                break
+            else:
+                raise ValidationError('Correo no valido')
 
 
 class RegisterExtra(Schema):
